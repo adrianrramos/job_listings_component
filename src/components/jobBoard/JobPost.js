8a5306e9056e435f4715b01246015902c436a812
Adrian Ramos <ramosadrian56@gmail.com>
2020-06-20 12:24:01 -0700

    mobile layout

--- src/components/jobBoard/JobPost.js
+++ src/components/jobBoard/JobPost.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import Icon from './Icon';
-import Description from './Description';
-import Tags from './Tags';
+import React from "react";
+import Icon from "./Icon";
+import Description from "./Description";
+import Tags from "./Tags";
 
-const JobPost = ({ 
-    avi, 
-    company, 
-    newPost, 
-    position, 
-    featured, 
-    postedAt, 
-    contract, 
-    location, 
-    tags,
-    onTagSelect 
+const JobPost = ({
+  avi,
+  company,
+  newPost,
+  position,
+  featured,
+  postedAt,
+  contract,
+  location,
+  tags,
+  onTagSelect,
 }) => {
-    return (
-        <div className="job-post">
-            <div className="icon-description-container">
-                <Icon avi={avi} alt={company}/>
-                <Description 
-                    company={company}
-                    newPost={newPost}
-                    position={position}
-                    featured={featured}
-                    postedAt={postedAt}
-                    contract={contract}
-                    location={location}
-                />
-            </div>
-            <div className="tags-container">
-                <Tags tags={tags} onTagSelect={onTagSelect}/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="job-post">
+      <div className="icon-description-container">
+        <Icon avi={avi} alt={company} />
+        <Description
+          company={company}
+          newPost={newPost}
+          position={position}
+          featured={featured}
+          postedAt={postedAt}
+          contract={contract}
+          location={location}
+        />
+      </div>
+      <div className="mobile-hr" />
+      <div className="tags-container">
+        <Tags tags={tags} onTagSelect={onTagSelect} />
+      </div>
+    </div>
+  );
+};
 
 export default JobPost;
